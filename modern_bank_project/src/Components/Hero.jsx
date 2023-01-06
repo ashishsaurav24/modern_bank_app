29944910f68eb308c7d805de4ed844f636846c4f
Ashish Saurav <ashishsaurav2410@gmail.com>
2023-01-06 18:44:21 +0530

    added stats component

--- modern_bank_project/src/Components/Hero.jsx
+++ modern_bank_project/src/Components/Hero.jsx
@@ -25,7 +25,6 @@ const Hero = () => {
             <span className="text-gradient">Generation</span>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
-            <GetStarted />
           </div>
         </div>
         <h1 className="font-poppins font-semibold ss:text-[62px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
